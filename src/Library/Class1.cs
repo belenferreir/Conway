8e8e6f9807692b2a37659e55c22d96b2b596037e
belenferreir <beluferrerey@gmail.com>
2023-04-19 18:32:41 -0300

    Ejercico Conway

--- src/Library/Class1.cs
+++ src/Library/Class1.cs
@@ -2,7 +2,112 @@
 
 namespace PII_Game_Of_Life
 {
-    public class Class1
+    public class Lógica
     {
+        bool[,] gameBoard = /* contenido del tablero */;
+        int boardWidth = gameBoard.GetLength(0);
+        int boardHeight = gameBoard.GetLength(1);
+
+        bool[,] cloneboard = new bool[boardWidth, boardHeight];
+        for (int x = 0; x < boardWidth; x++)
+        {
+            for (int y = 0; y < boardHeight; y++)
+            {
+                int aliveNeighbors = 0;
+                for (int i = x-1; i<=x+1;i++)
+                {
+                    for (int j = y-1;j<=y+1;j++)
+                    {
+                        if(i>=0 && i<boardWidth && j>=0 && j < boardHeight && gameBoard[i,j])
+                        {
+                            aliveNeighbors++;
+                        }
+                    }
+                }
+                if(gameBoard[x,y])
+                {
+                    aliveNeighbors--;
+                }
+                if (gameBoard[x,y] && aliveNeighbors < 2)
+                {
+                    //Celula muere por baja población
+                    cloneboard[x,y] = false;
+                }
+                else if (gameBoard[x,y] && aliveNeighbors > 3)
+                {
+                    //Celula muere por sobrepoblación
+                    cloneboard[x,y] = false;
+                }
+                else if (!gameBoard[x,y] && aliveNeighbors == 3)
+                {
+                    //Celula nace por reproducción
+                    cloneboard[x,y] = true;
+                }
+                else
+                {
+                    //Celula mantiene el estado que tenía
+                    cloneboard[x,y] = gameBoard[x,y];
+                }
+            }
+        }
+        gameBoard = cloneboard;
     }
+
+    public class Tablero
+    {
+
+    }
+
+    public class ImportadorArchivos
+    {
+        string url = "ruta del archivo";
+        string content = File.ReadAllText(url);
+        string[] contentLines = content.Split('\n');
+        bool[,] board = new bool[contentLines.Length, contentLines[0].Length];
+        for (int  y=0; y<contentLines.Length;y++)
+        {
+            for (int x=0; x<contentLines[y].Length; x++)
+            {
+                if(contentLines[y][x]=='1')
+                {
+                    board[x,y]=true;
+                }
+            }
+        }
+
+    }
+    
+    public class DibujarTablero
+    {
+        bool[,] b //variable que representa el tablero
+        int width //variabe que representa el ancho del tablero
+        int height //variabe que representa altura del tablero
+        while (true)
+        {
+            Console.Clear();
+            StringBuilder s = new StringBuilder();
+            for (int y = 0; y<height;y++)
+            {
+                for (int x = 0; x<width; x++)
+                {
+                    if(b[x,y])
+                    {
+                        s.Append("|X|");
+                    }
+                    else
+                    {
+                        s.Append("___");
+                    }
+                }
+                s.Append("\n");
+            }
+            Console.WriteLine(s.ToString());
+            //=================================================
+            //Invocar método para calcular siguiente generación
+            //=================================================
+            Thread.Sleep(300);
+        }
+    }
+
+    
 }
